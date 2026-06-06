bcd206f9375262332c6a09dc1a2c6cd0706f98cf
vvostryakov <vvostryakov@users.noreply.github.com>
2026-06-06 17:22:01 +0000

    deploy: 700dd99ee90636e87d61cd8d36abe75bb357c958

--- version.js
+++ version.js
@@ -1,5 +1,5 @@
 window.APP_VERSION = {
-  label: '3077dff',
-  commit: '3077dffd67b30e6008df1346578588fa7b4fbc1d',
-  builtAt: '2026-06-06T17:21:58Z',
+  label: '700dd99',
+  commit: '700dd99ee90636e87d61cd8d36abe75bb357c958',
+  builtAt: '2026-06-06T17:22:00Z',
 };
